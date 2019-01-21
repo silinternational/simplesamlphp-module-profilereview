b16124b3bb91113665d1276d523a2aa9e702f0a1
Steven Schram <steve_schram@sil.org>
2019-01-21 12:58:07 -0700

    use feature branch of ssp-base

--- development/symlink.sh
+++ development/symlink.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 if [ -d "/data/vendor/simplesamlphp/simplesamlphp/modules/profilereview" ]; then
-  exit
+  mv /data/vendor/simplesamlphp/simplesamlphp/modules/profilereview /data/vendor/simplesamlphp/simplesamlphp/modules/profilereview#
+  rm /data/vendor/simplesamlphp/simplesamlphp/modules/profilereview#/default-enable
 fi
 
 ln -s /profilereview /data/vendor/simplesamlphp/simplesamlphp/modules/
