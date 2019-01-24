3028ec11e6753c128b9c7cfc9519485334965e12
Steven Schram <steve_schram@sil.org>
2019-01-24 09:07:38 -0700

    volume root directory into simplesamlphp/modules

--- development/symlink.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -d "/data/vendor/simplesamlphp/simplesamlphp/modules/profilereview" ]; then
-  mv /data/vendor/simplesamlphp/simplesamlphp/modules/profilereview /data/vendor/simplesamlphp/simplesamlphp/modules/profilereview#
-  rm /data/vendor/simplesamlphp/simplesamlphp/modules/profilereview#/default-enable
-fi
-
-ln -s /profilereview /data/vendor/simplesamlphp/simplesamlphp/modules/
